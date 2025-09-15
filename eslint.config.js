5e18dd5c056316512018bddfe9c8faf3aed6149b
Shiramizu_Junya <jyunya.pg.7231@gmail.com>
2025-09-15 13:50:15 +0900

    fix: 環境構築

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
+// eslint.config.js
 import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
+import hooks from 'eslint-plugin-react-hooks'
+import a11y from 'eslint-plugin-jsx-a11y'
+import globals from 'globals'
+import prettier from 'eslint-config-prettier'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist', 'build', 'coverage', 'eslint.config.js'] },
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: { react, 'react-hooks': hooks, a11y },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'react/jsx-key': 'error',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
   },
-])
+  prettier,
+]
